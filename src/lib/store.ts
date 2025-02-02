67396009d29e7894756cdf2aeebd8e8a894bea24
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-02-02 20:37:08 +0530

    switch to laszyStore and update config for esnext to build on windows

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,6 +1,6 @@
 import { DB_ORDER_ENUM } from './constants';
 
-import { load } from '@tauri-apps/plugin-store';
+import { LazyStore } from '@tauri-apps/plugin-store';
 import { writable } from 'svelte/store';
 
 export const feeds_store = writable([]);
@@ -20,4 +20,4 @@ export const is_loading_feed = writable(false);
 export const is_loading_posts = writable(false);
 export const is_loading_post_content = writable(false);
 
-export const user_settings = await load('settings.json', { autoSave: true });
+export const user_settings = new LazyStore('settings.json', { autoSave: true });
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  build: {
+    target: 'esnext'
+  },
   plugins: [sveltekit()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
