2d35cff3c715281d6de3704b29959fef9b869bbf
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-02-11 20:00:52 +0530

    rust optimization + removed minify in release

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   build: {
-    minify: false,
     target: 'esnext'
   },
   plugins: [
