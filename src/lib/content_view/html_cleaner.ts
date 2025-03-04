17a9276ae272a8047dbb2438f5bdb147ec682c8d
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-03-05 01:27:17 +0530

    handle default text for failed image load in content view

--- src/lib/content_view/html_cleaner.ts
+++ src/lib/content_view/html_cleaner.ts
@@ -1,15 +1,32 @@
+const mergeURLs = (baseUrl:string, src:string) => {
+    if (!baseUrl) return src;
+
+    for (let i = 0; i < baseUrl.length; i++) {
+      if (src.startsWith(baseUrl.slice(i))) {
+        return baseUrl + src.slice(baseUrl.slice(i).length);
+      }
+    }
+    return baseUrl + src;
+};
+
 export const add_missing_src = (html:string, url:string="") => {
+    const fallbackText = "Unable to load image!"
+
     // Match all img tags and update src if necessary
     html = html.replace(/<img\s+([^>]*?)src=["'](?!https?:\/\/)([^"']+?)["']([^>]*?)>/gi, (match, before, src, after) => {
-        return `<img ${before}src="${url}${src}"${after}>`;
+        const newSrc = mergeURLs(url, src);
+        return `<img ${before}src="${newSrc}" onerror="this.style.display='none'; this.nextElementSibling.style.display='inline';" ${after}>
+        <span style="display:none;">${fallbackText}</span>`;
     });
 
     html = html.replace(/<audio\s+([^>]*?)src=["'](?!https?:\/\/)([^"']+?)["']([^>]*?)>/gi, (match, before, src, after) => {
-        return `<audio ${before}src="${url}${src}"${after}>`;
+        const newSrc = mergeURLs(url, src);
+        return `<audio ${before}src="${newSrc}"${after}>`;
     });
 
     html = html.replace(/<video\s+([^>]*?)src=["'](?!https?:\/\/)([^"']+?)["']([^>]*?)>/gi, (match, before, src, after) => {
-        return `<video ${before}src="${url}${src}"${after}>`;
+        const newSrc = mergeURLs(url, src);
+        return `<video ${before}src="${newSrc}"${after}>`;
     });
 
     return html;
