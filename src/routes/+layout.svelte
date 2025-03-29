353174010781e9a8542e17b89539d3e3a98f5298
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-03-29 13:43:37 +0530

    restructured home layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
     import "../app.css"
-    
+    import Titlebar from "$lib/components/titlebar.svelte";
+    import { themeMode } from "$lib/store";
+
+    let { children } = $props();
 </script>
 
 
-<slot />
+<div class={$themeMode}>
+    <Titlebar />
+    {@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script>
-  import Titlebar from "$lib/components/titlebar.svelte";
   import HomeListView from "$lib/pages/home_list_view/HomeListView.svelte";
-  import { themeMode } from "$lib/store";
 </script>
 
-<div class={$themeMode}>
-  <Titlebar />
-  <HomeListView/>
-</div>
+<HomeListView/>
