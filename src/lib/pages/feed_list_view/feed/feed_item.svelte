6217ac81baafcfbaf1340f15e7296909607f96bd
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-04-30 18:17:34 +0530

    folder open icon status

--- src/lib/pages/feed_list_view/feed/feed_item.svelte
+++ src/lib/pages/feed_list_view/feed/feed_item.svelte
@@ -14,9 +14,11 @@
     import { NO_OF_POST_PULLS_PER_TIME, FEED_VIEW, FEED_TYPE } from "$lib/constants";
 
     import Fa from "svelte-fa";
-    import { faStar } from "@fortawesome/free-solid-svg-icons";
+    import { faStar, faRss, faFolderOpen, faFolder } from "@fortawesome/free-solid-svg-icons";
 
     let { id, title, url, favicon, type, parent } = $props();
+
+    let folderOpen = $state(false);
 
     const count = $derived($feed_unread_post_count[id]);
 
@@ -37,6 +39,7 @@
             }
         } else if(type == FEED_TYPE.FOLDER){
             // TODO: Fetch different feed ids for the parent folder
+            folderOpen = !folderOpen;
         }
     };
 </script>
@@ -74,18 +77,14 @@
         </svg>
     {:else if id == -2}
         <Fa icon={faStar} size="lg" />
+    {:else if type == FEED_TYPE.FOLDER}
+        {#if folderOpen}
+            <Fa icon={faFolderOpen} size="lg" />
+        {:else}
+            <Fa icon={faFolder} size="lg" />
+        {/if}
     {:else}
-        <!-- Default Icon -->
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="size-6"
-        >
-            <path
-                d="M3.75 3A1.75 1.75 0 0 0 2 4.75v3.26a3.235 3.235 0 0 1 1.75-.51h12.5c.644 0 1.245.188 1.75.51V6.75A1.75 1.75 0 0 0 16.25 5h-4.836a.25.25 0 0 1-.177-.073L9.823 3.513A1.75 1.75 0 0 0 8.586 3H3.75ZM3.75 9A1.75 1.75 0 0 0 2 10.75v4.5c0 .966.784 1.75 1.75 1.75h12.5A1.75 1.75 0 0 0 18 15.25v-4.5A1.75 1.75 0 0 0 16.25 9H3.75Z"
-            />
-        </svg>
+        <Fa icon={faRss} size="lg" />
     {/if}
 
     {#if !$minimize_feeds}
