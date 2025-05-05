3d425332ce77da4524581477bcea2364a34a20bd
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-05-06 00:13:27 +0530

    revert code for splashscreen

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -26,18 +26,11 @@ async fn set_complete(
     // Check if both tasks are completed
     if state_lock.frontend_task {
         // Setup is complete, we can close the splashscreen
-        if let Some(splash_window) = app.get_webview_window("splashscreen") {
-            splash_window.close().unwrap();
-        } else {
-            println!("Splashscreen window not found");
-        }
-
         // and unhide the main window!
-        if let Some(main_window) = app.get_webview_window("main") {
-            main_window.show().unwrap();
-        } else {
-            println!("main window not found");
-        }
+        let splash_window = app.get_webview_window("splashscreen").unwrap();
+        let main_window = app.get_webview_window("main").unwrap();
+        splash_window.close().unwrap();
+        main_window.show().unwrap();
     }
     Ok(())
 }
