d2c60313388136493eb6772f85c2f7ba26c75960
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-05-06 03:06:02 +0530

    fix titlebar styling

--- src/lib/components/titlebar.svelte
+++ src/lib/components/titlebar.svelte
@@ -1,54 +1,41 @@
 <script>
-    import { getCurrentWindow } from '@tauri-apps/api/window';
+    import { getCurrentWindow } from "@tauri-apps/api/window";
+
+    import Fa from "svelte-fa";
+    import {
+        faWindowMinimize,
+        faWindowMaximize,
+        faWindowClose,
+    } from "@fortawesome/free-solid-svg-icons";
 
     const appWindow = getCurrentWindow();
 </script>
 
 <!-- svelte-ignore a11y_consider_explicit_label -->
-<div data-tauri-drag-region class="bg-background1 titlebar text-text1">
-    <button class="titlebar-button" id="titlebar-minimize" onclick={appWindow.minimize}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M20 14H4v-4h16"/></svg>
+<div data-tauri-drag-region class="titlebar">
+    <button class="btn btn-ghost btn-xs" onclick={appWindow.minimize}>
+        <Fa style="" icon={faWindowMinimize} size="lg" title="Minimize" />
     </button>
-    <button class="titlebar-button" id="titlebar-maximize" onclick={appWindow.toggleMaximize}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 4h16v16H4zm2 4v10h12V8z"/></svg>
+
+    <button class="btn btn-ghost btn-xs" onclick={appWindow.toggleMaximize}>
+        <Fa icon={faWindowMaximize} size="lg" title="Maximize" />
     </button>
-    <button class="titlebar-button" id="titlebar-close" onclick={appWindow.close}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"/></svg>
+
+    <button class="btn btn-ghost btn-xs" onclick={appWindow.close}>
+        <Fa icon={faWindowClose} size="lg" title="Exit" />
     </button>
 </div>
 
 <style>
-.titlebar {
-    height: var(--titlebar-height);
-    user-select: none;
-    display: flex;
-    justify-content: flex-end;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 1000;
-}
-
-.titlebar-button {
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    width: var(--titlebar-height);
-    height: var(--titlebar-height);
-    user-select: none;
-    -webkit-user-select: none;
-    border: none;
-    background: transparent;
-    cursor: pointer;
-}
-
-.titlebar-button:hover {
-    background: #5C6BC0;
-}
-
-.titlebar img {
-    width: 16px;
-    height: 16px;
-}
+    .titlebar {
+        height: var(--titlebar-height);
+        user-select: none;
+        display: flex;
+        justify-content: flex-end;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 1000;
+    }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,23 @@
 <script>
-    import "../app.css"
-    import Titlebar from "$lib/components/titlebar.svelte";
-    import { themeMode, is_loading_splashscreen } from "$lib/store";
-    import "$lib/pages/app_loader";
-    import { appLoader } from "$lib/pages/app_loader";
+    import "../app.css";
     import { onMount } from "svelte";
-    
+    import { appLoader } from "$lib/pages/loading_page/app_loader";
+    import Titlebar from "$lib/components/titlebar.svelte";
+    import { is_loading_splashscreen } from "$lib/store";
+    import AppLoadScreen from "$lib/pages/loading_page/app_load_screen.svelte";
+
     let { children } = $props();
 
     onMount(async () => {
         await appLoader();
     });
-
 </script>
 
-
-<div class={$themeMode}>
+<div>
+    <Titlebar />
     {#if !$is_loading_splashscreen}
-        <Titlebar />
+        {@render children()}
+    {:else}
+        <AppLoadScreen/>
     {/if}
-
-    {@render children()}
 </div>
