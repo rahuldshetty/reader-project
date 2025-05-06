448262f59c58e9986de09c0be69d5fe555628b3b
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-05-06 21:05:02 +0530

    title bar styling and layout

--- src/lib/components/titlebar.svelte
+++ src/lib/components/titlebar.svelte
@@ -11,30 +11,18 @@
     const appWindow = getCurrentWindow();
 </script>
 
-<div data-tauri-drag-region class="titlebar">
-    <button class="btn btn-ghost btn-xs" onclick={appWindow.minimize}>
-        <Fa icon={faWindowMinimize} size="lg" title="Minimize" />
-    </button>
+<div data-tauri-drag-region class="titlebar fixed top-0 left-0 right-0 w-full flex justify-end items-centerpy-2 rounded-t-lg">
+    <div class="flex items-center space-x-2">
+        <button class="btn btn-ghost btn-xs" onclick={appWindow.minimize}>
+            <Fa icon={faWindowMinimize} size="lg" title="Minimize" />
+        </button>
 
-    <button class="btn btn-ghost btn-xs" onclick={appWindow.toggleMaximize}>
-        <Fa icon={faWindowRestore} size="lg" title="Maximize" />
-    </button>
+        <button class="btn btn-ghost btn-xs" onclick={appWindow.toggleMaximize}>
+            <Fa icon={faWindowRestore} size="lg" title="Maximize" />
+        </button>
 
-    <button class="btn btn-ghost btn-xs" onclick={appWindow.close}>
-        <Fa icon={faXmark} size="lg" title="Exit" />
-    </button>
+        <button class="btn btn-ghost btn-xs" onclick={appWindow.close}>
+            <Fa icon={faXmark} size="lg" title="Exit" />
+        </button>
+    </div>
 </div>
-
-<style>
-    .titlebar {
-        height: var(--titlebar-height);
-        user-select: none;
-        display: flex;
-        justify-content: flex-end;
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        z-index: 1000;
-    }
-</style>
