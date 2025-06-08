c81c309d4ae9c0a6079be3e9f858a85888fcc9e3
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-06-08 15:45:22 +0530

    add home panel items

--- src/lib/pages/home_page/home_panel/home_panel.svelte
+++ src/lib/pages/home_page/home_panel/home_panel.svelte
@@ -1,65 +1,65 @@
 <script lang="ts">
-  const today = new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-  
-  	$effect(() => {
-      
-    });
+  import { fetch_posts } from "$lib/dao/post_db";
+  import type { PostResult } from "$lib/types";
+  import { onMount } from "svelte";
+  import LeftArticleCard from "./l_article_card.svelte";
+    import ArticleCardGallery from "./article_card_gallery.svelte";
+
+  const today = new Date().toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  let loading = $state(false);
+  let posts: PostResult[] = $state([]);
+
+  onMount(async () => {
+    loading = true;
+    posts = await fetch_posts();
+    loading = false;
+  });
 </script>
 
 <section class="">
   <div class="flex w-screen h-screen overflow-hidden">
-  <!-- Column 1: Medium -->
-  <div class="w-1/4 bg-base-200 p-4 overflow-hidden h-full flex flex-col">
-    <!-- Top Row -->
-    <div class="flex-1 bg-base-100 rounded-xl p-2">
-      <p class="text-lg font-bold">📄 Top Half</p>
-      <p>Content for row 1...</p>
+    <!-- Column 1: Medium -->
+    <div class="w-1/4 bg-base-200 p-4 overflow-hidden h-full flex flex-col pb-20">
+      {#each posts.slice(0, 3) as post}
+        <LeftArticleCard post={post} total={3}/>
+        <div class="divider"></div>
+      {/each}
     </div>
 
-    <!-- Divider -->
-    <div class="divider"></div>
-
-    <!-- Bottom Row -->
-    <div class="flex-1 bg-base-100 rounded-xl p-2">
-      <p class="text-lg font-bold">📄 Bottom Half</p>
-      <p>Content for row 2...</p>
-    </div>
-  </div>
-
-
-  <!-- Column 2: Large -->
-  <div class="w-2/4 bg-base-100 p-4 overflow-y-auto">
-    <!-- Content here -->
-    <div class="flex-1 bg-base-100 rounded-xl p-2">
-      <p class="text-lg font-bold">📄 Large Main Column</p>
-      <p>Content for row 1...</p>
-    </div>
-  </div>
-
-  <!-- Column 3: Small -->
-  <div class="w-1/4 bg-base-200 p-4 overflow-hidden h-full flex flex-col">
-    <!-- First Row -->
-    <div class="flex-1 bg-base-100 rounded-xl p-2">
-      <p class="text-lg font-bold">📄 Top Half</p>
-      <p>Content for row 1...</p>
+    <!-- Column 2: Large -->
+    <div class="w-2/4 bg-base-100 p-4 overflow-y-auto">
+      <ArticleCardGallery posts={posts.slice(3, 10)}/>
     </div>
 
-    <!-- Divider -->
-    <div class="divider"></div>
+    <!-- Column 3: Small -->
+    <div class="w-1/4 bg-base-200 p-4 overflow-hidden h-full flex flex-col">
+      <!-- First Row -->
+      <div class="flex-1 bg-base-100 rounded-xl p-2">
+        <p class="text-lg font-bold">📄 Top Half</p>
+        <p>Content for row 1...</p>
+      </div>
 
-    <!-- Second Row -->
-    <div class="flex-1 bg-base-100 rounded-xl p-2">
-      <p class="text-lg font-bold">📄 Bottom Half</p>
-      <p>Content for row 2...</p>
-    </div>
+      <!-- Divider -->
+      <div class="divider"></div>
 
-    <div class="divider"></div>
+      <!-- Second Row -->
+      <div class="flex-1 bg-base-100 rounded-xl p-2">
+        <p class="text-lg font-bold">📄 Bottom Half</p>
+        <p>Content for row 2...</p>
+      </div>
 
-    <!-- Third Row -->
-    <div class="flex-1 bg-base-100 rounded-xl p-2">
-      <p class="text-lg font-bold">📄 Bottom Half</p>
-      <p>Content for row 2...</p>
+      <div class="divider"></div>
+
+      <!-- Third Row -->
+      <div class="flex-1 bg-base-100 rounded-xl p-2">
+        <p class="text-lg font-bold">📄 Bottom Half</p>
+        <p>Content for row 2...</p>
+      </div>
     </div>
   </div>
-</div>
 </section>
---
+++ src/lib/pages/home_page/home_panel/l_article_card.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import type { PostResult } from "$lib/types";
+
+    const { post, total }: { post: PostResult | null, total: number } = $props();
+</script>
+
+{#if post == null}
+    <div class="card bg-base-100 image-full w-96 shadow-sm">
+        <div class="card-body">
+            <div class="skeleton h-4 w-28"></div>
+            <div class="skeleton h-32 w-full"></div>
+        </div>
+    </div>
+{:else}
+    <div class="card h-1/3 bg-base-100 image-full shadow-sm
+        transition-transform duration-200 hover:scale-[1.02] hover:shadow-md
+    ">
+        <figure>
+            <img
+            class="w-full h-full object-cover"
+            src={post.image}
+            alt={post.title}
+            />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">{post.title}</h2>
+            <div class="card-actions justify-end mt-auto">
+                <button class="btn btn-primary">Read More</button>
+            </div>
+        </div>
+    </div>
+{/if}
