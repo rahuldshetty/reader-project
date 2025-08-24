503a264dd1c006619453833a4f927f66c6b72502
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-08-24 23:49:55 +0530

    update calendar max date

--- src/lib/pages/home_page/home_panel/calendar.svelte
+++ src/lib/pages/home_page/home_panel/calendar.svelte
@@ -2,12 +2,14 @@
     import "cally";
     const {changePostByDate} = $props();
 
+    const today = new Date().toISOString().split("T")[0];
+
     const onChange = (event) => {
       changePostByDate(event.target.value);
     }
 </script>
 
-<calendar-date on:change={onChange} class="cally bg-base-100 border border-base-300 rounded-box">
+<calendar-date on:change={onChange} max={today} class="cally bg-base-100 border border-base-300 rounded-box">
   <svg aria-label="Previous" class="fill-current size-4" slot="previous" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path></svg>
   <svg aria-label="Next" class="fill-current size-4" slot="next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="m8.25 4.5 7.5 7.5-7.5 7.5"></path></svg>
   <calendar-month></calendar-month>
