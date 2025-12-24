a72f98f7b1632165b6173bd3ee27107e29d3d627
Rahul D Shetty <35rahuldshetty@gmail.com>
2025-12-24 12:58:28 +0530

    hide search in mobile view
    Signed-off-by: Rahul D Shetty <35rahuldshetty@gmail.com>

--- src/lib/pages/home_page/feed_panel/feed_search.svelte
+++ src/lib/pages/home_page/feed_panel/feed_search.svelte
@@ -1,35 +1,44 @@
 <script lang="ts">
-    import { ROOT_PARENT_FEED_ID } from "$lib/constants";
-    import { search_keywords, active_feed_id, filter_unread_posts } from "$lib/stores/app_store";
-    import { refresh_posts } from "../common";
+  import { ROOT_PARENT_FEED_ID } from "$lib/constants";
+  import {
+    search_keywords,
+    active_feed_id,
+    filter_unread_posts,
+    is_mobile,
+  } from "$lib/stores/app_store";
+  import { refresh_posts } from "../common";
 
-
-    const onSearchValueChange = async (e: Event) => {
-        const text = (e.target as HTMLInputElement).value;
-        if(text.trim().length == 0){
-            $search_keywords = [];
-        } else{
-            $filter_unread_posts = false;
-            $active_feed_id = ROOT_PARENT_FEED_ID;
-            $search_keywords = text.toLowerCase().trim().split(/\s+/);
-        }
-        await refresh_posts($active_feed_id);
+  const onSearchValueChange = async (e: Event) => {
+    const text = (e.target as HTMLInputElement).value;
+    if (text.trim().length == 0) {
+      $search_keywords = [];
+    } else {
+      $filter_unread_posts = false;
+      $active_feed_id = ROOT_PARENT_FEED_ID;
+      $search_keywords = text.toLowerCase().trim().split(/\s+/);
     }
-
+    await refresh_posts($active_feed_id);
+  };
 </script>
 
-<label class="input input-sm input-ghost">
-  <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-    <g
-      stroke-linejoin="round"
-      stroke-linecap="round"
-      stroke-width="2.5"
-      fill="none"
-      stroke="currentColor"
+{#if !$is_mobile}
+  <label class="input input-sm input-ghost">
+    <svg
+      class="h-[1em] opacity-50"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
     >
-      <circle cx="11" cy="11" r="8"></circle>
-      <path d="m21 21-4.3-4.3"></path>
-    </g>
-  </svg>
-  <input type="search" placeholder="Search" onchange={onSearchValueChange}/>
-</label>
+      <g
+        stroke-linejoin="round"
+        stroke-linecap="round"
+        stroke-width="2.5"
+        fill="none"
+        stroke="currentColor"
+      >
+        <circle cx="11" cy="11" r="8"></circle>
+        <path d="m21 21-4.3-4.3"></path>
+      </g>
+    </svg>
+    <input type="search" placeholder="Search" onchange={onSearchValueChange} />
+  </label>
+{/if}
